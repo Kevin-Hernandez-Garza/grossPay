59ff47926da1ef8993f7dfce94cdcd5356f1eebb
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-02-01 00:43:46 -0600

    first commit

---
+++ kevinHernandezGarza_lanOnePartOne.cpp
@@ -0,0 +1,25 @@
+//******************************************************************
+// Author: Kevin Hernandez-Garza
+// Assignment Number: Programming Assignment "1"
+// File Name: kevinHernandezGarza_lanOnePartOne.cpp
+// Course/Section: COSC 1337 Section -009
+// Due Date: 2/4/23
+// Instructor: Dr.Haytham Mohamed
+//
+// This program computes the gross-pay of an employee making $39,000/annually
+// that is made twice a month and bi-weekly. We will achieve this by computing
+// the annual gross and divide  it by 24 paychecks per year, and for a bi-weekly
+// pay it would be divided by 26. It then displays the inputs and results.
+//  ******************************************************************
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World";
+
+    return 0;
+}
